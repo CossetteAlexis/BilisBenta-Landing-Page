a7dbcb8b0847d2caccd62e3d9e9e5d1631fc61f9
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-06-24 10:25:09 +0800

    Created Landing Page UI

---
+++ layouts/topnav.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href=""><img src="../img/bilisbenta-logo.png" alt=""></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto font-weight-bold">
+        <li class="nav-item">
+          <a class="nav-link text-dark" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-dark" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-dark" href="#how-it-works">How it works</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-dark" href="#benefits">Benefits</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-dark" href="#package">Packages</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-dark" href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
---
+++ pages/home.php
@@ -0,0 +1,256 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+
+  </head>
+  <body>
+    @include('layouts.topnav')
+
+    <div class="container-fluid home" id="home">
+      <div class="home-content">
+        <div class="row">
+          <div class="col-sm-6 home-text">
+            <h1>Online Selling</h1>
+            <h1 class="text-blue">Made Easy.</h1>
+            <p>Your ONE STOP SHOP<br>for all your Online Selling.</p>
+            <button class="btn">Get Started</button>
+          </div>
+          <div class="col-sm-6 home-image">
+              <img class="img-responsive img-fluid" src="../img/1.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid about" id="about">
+      <div class="about-content">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <div class="about-text">
+              <h1>Looking for a new business ,
+                <br>ride the booming business of
+                <br>E-commerce
+              </h1>
+              <p class="pt-4">Sell on Lazada, Shopee or your own
+                <br>Shopify website.
+              </p>
+              <button class="btn">How it works</button>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6">
+              <img class="img-responsive img-fluid" src="../img/2.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid how-it-works" id="how-it-works">
+      <div class="row how-content text-center">
+        <div class="col-sm-12 how-text">
+          <h2 class="text-blue">We've brought more than 50,000 Products
+            <span class="text-dark">to sell in one Roof from different
+              <br>manufacturers and suppliers into one marketplace.
+            </span>
+          </h2>
+          <p class="pt-4">You can list and sell products without ever having to buy inventory up front
+            <br>or pack and ship orders
+          </p>
+        </div>
+        <div class="col-md-12 how-cards">
+          <div class="card ml-3">
+            <div class="card-body">
+              <div class="numberCircle">1</div>
+              <img class="card-img mt-4" src="../img/001-resume.png" alt="">
+              <p class="card-text pt-2">Sign up and login</p>
+            </div>
+          </div>
+          <div class="card ml-3">
+            <div class="card-body">
+              <div class="numberCircle">2</div>
+              <img class="card-img mt-3" src="../img/002-search.png" alt="">
+              <p class="card-text pt-2">Free browing on all the packages we offer</p>
+            </div>
+          </div>
+          <div class="card ml-3">
+            <div class="card-body">
+              <div class="numberCircle">3</div>
+              <img class="card-img mt-4" src="../img/003-bill.png" alt="">
+              <p class="card-text pt-2">Choose the package that best suits you</p>
+            </div>
+          </div>
+          <div class="card ml-3">
+           <div class="card-body">
+            <div class="numberCircle">4</div>
+            <img class="card-img mt-4" src="../img/004-checklist.png" alt="">
+            <p class="card-text pt-2">Review terms and condition</p>
+           </div>
+          </div>
+          <div class="card ml-3">
+            <div class="card-body">
+              <div class="numberCircle">5</div>
+              <img class="card-img mt-3" src="../img/005-form.png" alt="">
+              <p class="card-text pt-2">Fillup the requirements forms and details</p>
+            </div>
+          </div>
+          <div class="card ml-3">
+            <div class="card-body">
+              <div class="numberCircle">6</div>
+              <img class="card-img mt-4" src="../img/006-pay.png" alt="">
+              <p class="card-text pt-2">Choose a plan for payments and options</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="benefits" id="benefits">
+      <div class="row">
+        <div class="col benefits-ad">
+          <h4 class="text-blue">
+            <strong>REGISTER FOR FREE!</strong>
+            <span class="text-dark"><strong>No Credit Card Needed</strong></span>
+          </h4>
+        </div>
+        <div class="col">
+          <button class="btn float-right">Get Started</button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col benefits-content">
+          <h4 class="text-dark text-center">
+            <strong>Why you should choose 
+              <span class="text-blue">BilisBenta?</span>
+            </strong>
+          </h4>
+          <div class="container benefits-list">
+            <div class="row">
+              <div class="col-md-3 col-sm-12 pt-5">
+                <img src="../img/tick-mark.png" alt="" height="35px" width="35px">
+                <span class="pl-3">No manpower needed</span>
+              </div>
+              <div class="col-md-3 col-sm-12 pt-5">
+                <img src="../img/tick-mark.png" alt="" height="35px" width="35px">
+                <span class="pl-3">No warehouse rental</span>
+              </div>
+              <div class="col-md-3 col-sm-12 pt-5">
+                <img src="../img/tick-mark.png" alt="" height="35px" width="35px">
+                <span class="pl-3">No capital on inventory</span>
+              </div>
+              <div class="col-md-3 col-sm-12 pt-5">
+                <img src="../img/tick-mark.png" alt="" height="35px" width="35px">
+                <span class="pl-3">No overhead expenses</span>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-3 col-sm-12 pt-5">
+                <img src="../img/tick-mark.png" alt="" height="35px" width="35px">
+                <span class="pl-3">No accounting needed</span>
+              </div>
+              <div class="col-md-3 col-sm-12 pt-5">
+                <img src="../img/tick-mark.png" alt="" height="35px" width="35px">
+                <span class="pl-3">Great product sourcing</span>
+              </div>
+              <div class="col-md-3 col-sm-12 pt-5">
+                <img src="../img/tick-mark.png" alt="" height="35px" width="35px">
+                <span class="pl-3">Multi category products</span>
+              </div>
+              <div class="col-md-3 col-sm-12 pt-5">
+                <img src="../img/tick-mark.png" alt="" height="35px" width="35px">
+                <span class="pl-3">Everyone can sell</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid package text-center" id="package">
+      <div class="package-content">
+        <div class="row">
+          <div class="col">
+            <div class="package-text">
+              <h4><strong>We have 3 packages available!</strong></h4>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row package-cards">
+            <div class="col-md-4 col-sm-12">
+              <div class="card">
+                <div class="card-title"><h5>Basic</h5></div>
+                <div class="card-header"><h1>$25/mo</h1><span>(or $129 annualy)</span></div>
+                <div class="card-body">
+                  <p class="card-text">
+                    <br>Exclusive access to 
+                    <br>BilisBenta top Sellers
+                    <br>Elite Seller Report
+                    <br>All Premium Data Exports
+                    <br>(Amazon|Ebay|Shopify)
+                    <br>Custom Data Export
+                </p>
+                <button class="btn">BUY PACKAGE</button>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 col-sm-12">
+              <div class="card">
+                <div class="card-title"><h5>Standard</h5></div>
+                <div class="card-header"><h1>$45/mo</h1><span>(or $149 annualy)</span></div>
+                <div class="card-body">
+                  <p class="card-text">
+                      <br>Exclusive access to 
+                      <br>BilisBenta top Sellers
+                      <br>Elite Seller Report
+                      <br>All Premium Data Exports
+                      <br>(Amazon|Ebay|Shopify)
+                      <br>Custom Data Export
+                  </p>
+                  <button class="btn">BUY PACKAGE</button>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 col-sm-12">
+              <div class="card">
+                <div class="card-title"><h5>Premium</h5></div>
+                <div class="card-header"><h1>$55/mo</h1><span>(or $169 annualy)</span></div>
+                <div class="card-body">
+                  <p class="card-text">
+                    <br>Exclusive access to 
+                    <br>BilisBenta top Sellers
+                    <br>Elite Seller Report
+                    <br>All Premium Data Exports
+                    <br>(Amazon|Ebay|Shopify)
+                    <br>Custom Data Export
+                </p>
+                <button class="btn">BUY PACKAGE</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid contact" id="contact">
+      <div class="contact-text">
+        <h4>Online selling made easy</h4>
+        <p>
+        REGISTER FOR FREE
+        <br>
+        No Credit Card Needed
+      </p>
+      <button class="btn">Get Started</button>
+      </div>  
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
